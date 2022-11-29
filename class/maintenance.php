be458668097f0cab1ffb2643341532d90ca8a524
Jake Marfon Chito A. Azura <jmcaazura@gmail.com>
2022-11-30 01:59:41 +0800

    Validation, Update Date

--- class/maintenance.php
+++ class/maintenance.php
@@ -24,32 +24,37 @@ class Maintenance extends Base
         $blank++;
       }
     }
+    switch ($dropdown_name) {
+      case 'COMMODITY':
+        $table = 'tbl_comodity';
+        break;
+      case 'END USER':
+        $table = 'tbl_end_user';
+        break;
+      case 'MODE OF PROC':
+        $table = 'tbl_mode_of_proc';
+        break;
+      case 'PABAC':
+        $table = 'tbl_pabac';
+        break;
+      case 'PROGRAM MANAGER':
+        $table = 'tbl_program_manager';
+        break;
+    }
+    
+    if ($this->get_one("SELECT COUNT(*) AS count FROM $table WHERE name = '$dropdown_value' AND deleted_flag = 0")->count > 0) {
+      $result->result = $this->response_error("$dropdown_value already exist!", "Duplicate error!");
+      return $result;
+    }
+
 
     $this->start_transaction();
     try {
-      switch ($dropdown_name) {
-        case 'COMMODITY':
-          $table = 'tbl_comodity';
-          break;
-        case 'END USER':
-          $table = 'tbl_end_user';
-          break;
-        case 'MODE OF PROC':
-          $table = 'tbl_mode_of_proc';
-          break;
-        case 'PABAC':
-          $table = 'tbl_pabac';
-          break;
-        case 'PROGRAM MANAGER':
-          $table = 'tbl_program_manager';
-          break;
-      }
-
       $this->query("
         INSERT INTO 
           $table (`name`,`created_date`,`updated_date`,`deleted_flag`) 
           VALUES(UPPER('$dropdown_value'), NOW(), NOW(), 0)
-          ON DUPLICATE KEY UPDATE deleted_flag = 1
+          ON DUPLICATE KEY UPDATE deleted_flag = 0, updated_date = NOW()
       ");
 
       $this->commit_transaction();
@@ -87,14 +92,14 @@ class Maintenance extends Base
 
     $this->start_transaction();
     try {
-      $this->query("UPDATE $table SET `name` = '$dropdown_value' WHERE `id` = $id");
+      $this->query("UPDATE $table SET `name` = '$dropdown_value', `updated_date` = NOW() WHERE `id` = $id");
       $this->commit_transaction();
       $result->status = true;
       $result->result = $this->response_swal("$dropdown_name ID $id Updated Successfully!", 'Successfull!');
       return $result;
     } catch (mysqli_sql_exception $exception) {
       $this->roll_back();
-      $result->result = $this->response_error("Name $dropdown_value already exist!");
+      $result->result = $this->response_error("Name $dropdown_value already exist!", "Duplicate error!");
       return $result;
     }
   }
@@ -123,7 +128,7 @@ class Maintenance extends Base
 
     $this->start_transaction();
     try {
-      $this->query("UPDATE $table SET `deleted_flag` = 1 WHERE `id` = $id");
+      $this->query("UPDATE $table SET `deleted_flag` = 1, `updated_date` = NOW() WHERE `id` = $id");
       $this->commit_transaction();
       $result->status = true;
       $result->result = $this->response_swal("$dropdown_name ID $id Deleted Successfully!", 'Successfull!');
